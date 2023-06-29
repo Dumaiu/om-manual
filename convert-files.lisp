3822feeebdf1a5934372d9fa03117fe1569400ea
Dumaiu <dymaio@gmail.com>
2023-06-29 10:47:15 -0400

    convert-files.lisp,yhtml.lisp: Add (pandoc-command) from alt branch

--- convert-files.lisp
+++ convert-files.lisp
@@ -9,9 +9,10 @@
 	 :asdf-user)
   (:intern
    file-error
-	 run-program)
+   run-program)
   (:export
    *default-directory*
+   pandoc-command
    ))
 
 (in-package :om-manual-conversion)
@@ -19,24 +20,78 @@
 
 
 (defvar +root-directory+ (let ((dir
-								 (pathname-directory-pathname (system-definition-pathname (find-system :om-manual-conversion)))))
-                           (assert (directory-exists-p dir))
-                           dir))
+								   (pathname-directory-pathname (system-definition-pathname (find-system :om-manual-conversion)))))
+						   (assert (directory-exists-p dir))
+						   dir))
 
 (defparameter *default-directory*
   (merge-pathnames* "docs/om/om6-manual/co/" +root-directory+))
 
 (assert (directory-exists-p *default-directory*))
 
-; '#99 ; XXX
+(defparameter *manual* (merge-pathnames* "OM-User-Manual.html" *default-directory*))
+(assert (file-exists-p *manual*))
+
+(defparameter *manual.md* (make-pathname :type "md" :defaults *manual*))
+
+										; '#99 ; XXX
 '#:foo ; XXX
 ':foo
 
+(defun pandoc-command (&key input
+						 input-file
+						 in-fmt
+						 out-fmt
+						 in-type
+						 (out-type out-fmt)
+						 (output :string))
+  "Returns a list used by (pandoc), q.v."
+  (cond
+	(input-file
+	 (when input (assert (equalp input-file input)))
+	 (check-type input-file (or string pathname)))
+	(input
+		(assert (null input-file))
+	  (when (pathnamep input)
+		(setf input-file input))))
+
+  (assert out-fmt (out-fmt) "The :out-fmt key is required.")
+  (check-type out-fmt string)
+
+  (unless in-type
+	(setq in-type (or in-fmt
+					  (pathname-type input-file))))
+  (check-type in-type string)
+
+  (check-type out-fmt string)
+  (check-type out-type string)
+
+  (when (eq :file output)
+	(setq output (make-pathname :type out-type :defaults input-file))
+	;; (break "Modified output: ~A" output)
+	)
+
+  (let ((command `("pandoc"
+				   ,@(when in-fmt
+					   `("-f" ,in-fmt))
+				   "-t" ,out-fmt
+				   ,(ensure-list
+					 (when input-file
+					   (let* ((input-file-pathname (ensure-pathname input-file))
+							  (input-file~ (probe-file input-file-pathname))
+							  #|(expr (format nil "pandoc XXX")|#)
+						 (unless (file-exists-p input-file~)
+						   (error 'file-error :pathname input-file-pathname))
+						 (namestring input-file~)))))))
+
+	command))
+
 (defun html-string->md (input
-						&key (output :string)
-						  (in-fmt "html")
-						  (out-fmt "gfm")
-						  )
+						   &key (output :string)
+						 (in-fmt "html")
+						 (out-fmt "gfm")
+						 )
+  (declare (string input))
   (with-input (*standard-input* input)
 	(run-program (list "pandoc"
 					   "-f" in-fmt
@@ -50,8 +105,9 @@
 						(in-fmt "html")
 						(out-fmt "gfm")
 						(input-file
-						 (probe-file (ensure-pathname (pathname-name input-file~)
-													  :type in-fmt))))
+						 (or (file-exists-p input-file~)
+							 (probe-file (ensure-pathname (pathname-name input-file~)
+														  :type in-fmt)))))
   "By default, returns output as a string.  To write to a file, use `:output`, e.g.::
 	  (html-file->md \"00-Sommaire.html\" :output :file)
 
@@ -90,7 +146,7 @@
 			   :output output))
 
 (defun md-string->html (input
-						&key (output :string))
+						   &key (output :string))
   (with-input (*standard-input* input)
 	(run-program (list "pandoc"
 					   "-f" "gfm"
@@ -107,5 +163,9 @@
 
    (md-file->html "01-Presentation.md" :output :file)
 
+
+   (pandoc-command :input-file *manual.md*
+				   :in-fmt "gfm"
+				   :out-fmt "html")
 
    )
