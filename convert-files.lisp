415bf98c3c955e0583f12a3fd2f9e9731089a15d
Dumaiu <dymaio@gmail.com>
2023-06-29 16:14:48 -0400

    convert-files.lisp: Refactor; (html-file->md+-file)

--- convert-files.lisp
+++ convert-files.lisp
@@ -4,6 +4,55 @@
 
 ;; (defparameter *orig-manual.md.ystok* (parse-html *manual*))
 
+
+(defun html->sexp+ (file)
+  "Convert with modifications."
+  (let-1 sexp (parse-html file :callbacks `((:div . ,(λ div-form
+													   "Remove 'googleSearchFrom' `div` element."
+													   ;; (break "Arg: ~A" div-form)
+													   (match div-form
+														 ((list* (list* :div (plist :class  "googleSearchFrom")) _)
+														  ;; (break "class: ~A" class)
+														  ;; (break "Arg: ~A" div-form)
+														  (values nil t) ; delete element
+														  ))))
+											(:a . ,(λ anchor-form
+													 "* Remove 'Scenari' `<a>` at end of page.
+ * Change .html extensions to .md.
+"
+													 (match anchor-form
+													   ((list* (list* :a (plist :href "http://scenari-platform.org")) _)
+														(values nil t))  ; delete element
+													   ((guard (list* (list* :a (and plist
+																					 (plist :href href))) _)
+															   (string-equal "html"
+																			 (pathname-type href)))
+														(let-1 md-anchor (namestring (make-pathname :type "md"
+																									:defaults href))
+														  (setf (getf plist :href) md-anchor)
+														  ;; (break "HTML anchor: ~S" anchor-form)
+														  (values anchor-form t))))))
+											(:img . ,(λ img-form
+													   "Add alt-text to `<img>` tags."
+													   (match img-form
+														 ((guard (list* (list* :img (and plist-form
+																						 (plist :src src :alt alt-text)))
+																		_)
+																 (or (null alt-text)
+																	 (string-equal alt-text "")))
+														  ;; (break "Null-text img: ~A" img-form)
+														  ;; TODO: Plist setter; no-copy lens:
+														  (let-1 alt-text~ (namestring
+																			(make-pathname :name (pathname-name src)
+																						   :type (pathname-type src)))
+															(setf (getf plist-form :alt) alt-text~)
+															;; (rplacd (last plist-form) (list :alt alt-text~))
+															)
+														  ;; (break "Null-text img: ~S" plist-form)
+														  (values img-form t)))))
+											))
+	sexp))
+
 ''(
 
    (parse-html
@@ -17,80 +66,49 @@
 
 ;; (parse-html *manual*)
 
-;; 1.
-(defparameter *manual.ystok*
-  (parse-html *manual* :callbacks `((:div . ,(λ div-form
-											   "Remove 'googleSearchFrom' `div` element."
-											   ;; (break "Arg: ~A" div-form)
-											   (match div-form
-												 ((list* (list* :div (plist :class  "googleSearchFrom")) _)
-												  ;; (break "class: ~A" class)
-												  ;; (break "Arg: ~A" div-form)
-												  (values nil t) ; delete element
-												  ))))
-									(:a . ,(λ anchor-form
-											 "* Remove 'Scenari' `<a>` at end of page.
- * Change .html extensions to .md.
-"
-											 (match anchor-form
-											   ((list* (list* :a (plist :href "http://scenari-platform.org")) _)
-												(values nil t))  ; delete element
-											   ((guard (list* (list* :a (and plist
-																			 (plist :href href))) _)
-													   (string-equal "html"
-																	 (pathname-type href)))
-												(let-1 md-anchor (namestring (make-pathname :type "md"
-																							:defaults href))
-												  (setf (getf plist :href) md-anchor)
-												  ;; (break "HTML anchor: ~S" anchor-form)
-												  (values anchor-form t))))))
-									(:img . ,(λ img-form
-											   "Add alt-text to `<img>` tags."
-											   (match img-form
-												 ((guard (list* (list* :img (and plist-form
-																				 (plist :src src :alt alt-text)))
-																_)
-														 (or (null alt-text)
-															 (string-equal alt-text "")))
-												  ;; (break "Null-text img: ~A" img-form)
-												  ;; TODO: Plist setter; no-copy lens:
-												  (let-1 alt-text~ (namestring
-																	(make-pathname :name (pathname-name src)
-																				   :type (pathname-type src)))
-													(setf (getf plist-form :alt) alt-text~)
-													;; (rplacd (last plist-form) (list :alt alt-text~))
-													)
-												  ;; (break "Null-text img: ~S" plist-form)
-												  (values img-form t)))))
-									))
-  "Parse HTML to Lisp, making modifications.")
+''(
+   ;; 1.
+   (defparameter *manual.ystok* (html->sexp+ *manual*)
+	 "Parse HTML to Lisp, making modifications.")
 
 
 
-;; 2.
-(defparameter *manual.ystok.html*
-  (to-html *manual.ystok*)
-  "Convert back to HTML.  NB: the URLs still point to Markdown files!")
+   ;; 2.
+   (defparameter *manual.ystok.html*
+	 (to-html *manual.ystok*)
+	 "Convert back to HTML.  NB: the URLs still point to Markdown files!")
 
-;; 3. Generate Markdown file
-(html-string->md *manual.ystok.html* :output *manual.md*) ; *side-effect*
+   ;; 3. Generate Markdown file
+   (html-string->md *manual.ystok.html* :output *manual.md*) ; *side-effect*
+
+   )
+
+(defun html-file->md+-file (file)
+  "Write to new file."
+  (let* ((sexp (html->sexp+ file))
+		 (html+ (to-html sexp)))
+	(let-1 newfile (make-pathname :type "md" :defaults file)
+	  (html-string->md html+ :output newfile)
+	  newfile)))
+
+(html-file->md+-file *manual*) ; *test side-effect*
 
 ;; 4. Convert back to HTML:
 (let* ((html (md-file->html *manual.md*))
 	   ;; 5. Convert to Lisp, editing anchors:
 	   (sexp (parse-html html :callbacks `((:a . ,(λ anchor-form
-													 " * Change '.md' extensions to '.html'.
+													" * Change '.md' extensions to '.html'.
 "
-													 (match anchor-form
-													   ((guard (list* (list* :a (and plist
-																					 (plist :href href))) _)
-															   (string-equal "md"
-																			 (pathname-type href)))
-														(let-1 new-href (namestring (make-pathname :type "html"
-																								   :defaults href))
-														  (setf (getf plist :href) new-href)
-														  ;; (break "HTML anchor: ~S" anchor-form)
-														  (values anchor-form t)))))))))
+													(match anchor-form
+													  ((guard (list* (list* :a (and plist
+																					(plist :href href))) _)
+															  (string-equal "md"
+																			(pathname-type href)))
+													   (let-1 new-href (namestring (make-pathname :type "html"
+																								  :defaults href))
+														 (setf (getf plist :href) new-href)
+														 ;; (break "HTML anchor: ~S" anchor-form)
+														 (values anchor-form t)))))))))
 	   (html~ (to-html sexp)))
 
   ;; 6. Convert Lisp to HTML.
